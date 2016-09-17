7ddb6cbf163068ff39bfe06c75912810f4865703
Adolfo Tamayo <adolfo@inka-labs.com>
2016-09-16 19:59:49 -0500

    times view

--- index.js
+++ index.js
@@ -11,15 +11,24 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+    response.render('pages/index');
 });
 
 app.get('/cool', function(request, response) {
-  response.send(cool());
+    response.send(cool());
+});
+
+app.get('/times', function(request, response){
+    var result = '';
+    var times = process.env.TIMES || 5;
+    for (var i = 0; i < times; i++) {
+        result += i + ' ';
+    }
+    response.send(result);
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+    console.log('Node app is running on port', app.get('port'));
 });
 
 
